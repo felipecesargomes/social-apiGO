0808faffc0c7902a7e3089f4fb9558af148c9723
Felipe <www.felipecesargomes@gmail.com>
2025-01-11 17:24:33 -0300

    feat: create struct configuration of routers specify

---
+++ src/router/routers/routers.go
@@ -0,0 +1,25 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Routers struct {
+	URI              string
+	Method           string
+	Function         func(http.ResponseWriter, *http.Request)
+	VerificationAuth bool
+}
+
+func ConfigRouters(r *mux.Router) *mux.Router {
+	routers := userRouters
+
+	for _, router := range routers {
+		r.HandleFunc(router.URI, router.Function).Methods(router.Method)
+	}
+
+	return r
+
+}
