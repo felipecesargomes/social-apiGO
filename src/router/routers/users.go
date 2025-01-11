9c8b56e762b3f98ce739100c7acb6329c9b9ad7f
Felipe <www.felipecesargomes@gmail.com>
2025-01-11 17:22:42 -0300

    feat: create routers user specified

---
+++ src/router/routers/users.go
@@ -0,0 +1,42 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/felipecesargomes/social-apiGO/src/controllers"
+)
+
+const userEndpoint = "/users"
+
+var userRouters = []Routers{
+	{
+		URI:              userEndpoint + "/create",
+		Method:           http.MethodPost,
+		Function:         controllers.CreateUser,
+		VerificationAuth: false,
+	},
+	{
+		URI:              userEndpoint + "/{id}",
+		Method:           http.MethodGet,
+		Function:         controllers.FindUser,
+		VerificationAuth: false,
+	},
+	{
+		URI:              userEndpoint,
+		Method:           http.MethodGet,
+		Function:         controllers.FindUsers,
+		VerificationAuth: false,
+	},
+	{
+		URI:              userEndpoint,
+		Method:           http.MethodPut,
+		Function:         controllers.UpdateUser,
+		VerificationAuth: false,
+	},
+	{
+		URI:              userEndpoint,
+		Method:           http.MethodDelete,
+		Function:         controllers.DeleteUser,
+		VerificationAuth: false,
+	},
+}
