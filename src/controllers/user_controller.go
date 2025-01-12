7f849c43015c0c43c17e25281e68936f2fe6eaae
Felipe <www.felipecesargomes@gmail.com>
2025-01-12 20:47:31 -0300

    feat: add funcionality to create user in controller

--- src/controllers/user_controller.go
+++ src/controllers/user_controller.go
@@ -1,11 +1,40 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/felipecesargomes/social-apiGO/src/config"
+	"github.com/felipecesargomes/social-apiGO/src/models"
+	"github.com/felipecesargomes/social-apiGO/src/repositories"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create user"))
+	requestBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user models.User
+
+	if err := json.Unmarshal(requestBody, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db := config.Connection
+
+	repository := repositories.NewRepositoryUser(db)
+	userID, err := repository.CreateUser(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Id insert: %d", userID)))
+
 }
 
 func FindUser(w http.ResponseWriter, r *http.Request) {
