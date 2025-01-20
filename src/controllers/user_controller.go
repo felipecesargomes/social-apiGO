1067257c68fb8d7db925b378393e2e2af8da2a0e
Felipe <www.felipecesargomes@gmail.com>
2025-01-19 21:07:17 -0300

    refactor: refactor user controller

--- src/controllers/user_controller.go
+++ src/controllers/user_controller.go
@@ -29,7 +29,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := user.Prepare(); err != nil {
+	if err := user.Prepare("add"); err != nil {
 		responses.Err(w, http.StatusBadRequest, err)
 		return
 	}
@@ -61,11 +61,59 @@ func FindUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Update user"))
+	paramenters := mux.Vars(r)
+	userID, err := strconv.ParseUint(paramenters["id"], 10, 64)
+	if err != nil {
+		responses.Err(w, http.StatusBadRequest, err)
+		return
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.Err(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var user models.User
+	if err := json.Unmarshal(body, &user); err != nil {
+		responses.Err(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := user.Prepare("update"); err != nil {
+		responses.Err(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db := config.Connection
+
+	repository := repositories.NewRepositoryUser(db)
+	if err := repository.UpdateUser(userID, user); err != nil {
+		responses.Err(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusNoContent, nil)
+
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Delete user"))
+	parameters := mux.Vars(r)
+
+	userID, err := strconv.ParseUint(parameters["id"], 10, 64)
+	if err != nil {
+		responses.Err(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db := config.Connection
+
+	repository := repositories.NewRepositoryUser(db)
+	if err := repository.DeleteUser(userID); err != nil {
+		responses.Err(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusNoContent, nil)
 }
 
 func FindUser(w http.ResponseWriter, r *http.Request) {
