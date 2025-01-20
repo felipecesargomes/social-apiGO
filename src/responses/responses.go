f99e449bdb120051fe3ec60559a18da61803f5fc
Felipe <www.felipecesargomes@gmail.com>
2025-01-19 21:10:20 -0300

    refactor: adjust when body is empty

--- src/responses/responses.go
+++ src/responses/responses.go
@@ -10,6 +10,11 @@ func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
+	if data == nil {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		log.Fatal(err)
 	}
